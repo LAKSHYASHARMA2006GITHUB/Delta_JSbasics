1223ed4d4b7bcecc713847e693a7e146d5230ce2
lakshya sharma <lakshyasharma11042006@gmail.com>
2024-08-18 08:59:09 +0530

    commited2

---
+++ variables.js
@@ -0,0 +1,23 @@
+console.log("lakshya");
+console.log("hello i am javascript");
+
+
+const email = "lakshya@gmail.com";
+const mobno = 2342523532;
+let names = "lakshya"
+let age = 19;
+console.log(email, age);
+/*it prints the output in the table format */
+console.table([email, mobno, names, age]);
+/* it prints a warning */
+console.warn("details must be changed")
+
+/*
+stop using the variable var too much insted of use the variable
+keywords const and let to declare the variable
+*/
+// var ages = 20;
+// var ages  =21
+// console.log(ages);
+
+
