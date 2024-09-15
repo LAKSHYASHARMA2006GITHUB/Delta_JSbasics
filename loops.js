967db2e6ee1e306ba69bbad4e696d7353a9dcbfc
lakshya sharma <lakshyasharma11042006@gmail.com>
2024-09-15 23:12:47 +0530

    commited on 15-09-2024

---
+++ loops.js
@@ -0,0 +1,44 @@
+// for(let i=1;i<=15;i++){
+//   if(i%2!=0){
+//     console.log(`the odd numbers are ${i}`)
+//   }
+//   else{
+//     console.log(`even numbers are ${i}`);
+//   }
+// }
+
+// for(let i=1;i<=15;i=i+2){
+//     console.log(`the odd numbers are ${i}`)
+// }
+// console.log("\n");
+// for (let i = 2; i <= 10; i = i + 2) {
+//     console.log(`the even numbers are ${i}`)
+// }
+
+// for(let i=1;;i++){
+//     console.log(i);
+// }
+
+// for (let i = 1; i <= 3; i++) {
+//     console.log("outer loop");
+//     for (let j = 1; j <= 3; j++) {
+
+//         console.log(j);
+
+//     }
+//     console.log("inner loop")
+// }
+
+// while loop
+const movie = "avtar";
+let guess = prompt("enter the movie in your guess");
+
+while ((guess != movie) && (guess != "quit")) {
+   guess =  prompt("wrong movie take another guess:");
+}
+
+if(guess ==  movie){
+    console.log("you guess write!");
+}else if(guess == "quit"){
+    console.log("you quit the game");
+}
