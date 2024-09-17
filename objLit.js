acf390a2498d8ba88c5b549052a2830250f69ba1
lakshya sharma <lakshyasharma11042006@gmail.com>
2024-09-17 14:22:01 +0530

    commited on 17-09-2024

---
+++ objLit.js
@@ -0,0 +1,39 @@
+// Object literals in javascript .
+const student = {
+    name:"lakshya",
+    age:"24",
+    Salary:2400000
+};
+console.log(student);
+
+let item = {
+    name:"bottle",
+    weight:"100gm",
+    color:["red","pink"]
+}
+console.log(item);
+
+//twitter concept
+
+let twit= {
+    username:"lakshya@5727",
+    content:"loream@ found the criminal",
+    likes:45343,
+    reposts:234,
+    tags:35
+}
+
+console.log(twit.username);
+console.log(twit.content);
+console.log(twit.tags);
+
+
+const obj = {
+    1:"a",
+    2:"b",
+    3:'c',
+    null: "d",
+    undefined:"e",
+    apple:"f"
+}
+console.log(obj.null);
