5e37376498db99598bd274d38b1debffd7bfa922
lakshya sharma <lokmanh2148t@gmail.com>
2024-09-25 16:43:43 +0530

    commited on 25-09-2024

--- DOM/app.js
+++ DOM/app.js
@@ -1,8 +1,10 @@
+
+// selcting the element the className
 let oldImg = document.getElementsByClassName("oldImg");
 
 
-for(let i = 0;i<oldImg.length;i++){
-    oldImg[i].src =  "assets/spiderman_img.png";
+for (let i = 0; i < oldImg.length; i++) {
+    oldImg[i].src = "assets/spiderman_img.png";
     console.log(`the vlaue of image are ${i} is changed`)
 }
 
@@ -12,38 +14,40 @@ para.innerHTML = "<b> hi i am peter parkor </b>";
 
 // gettters and setters in DOM
 
-let img = document.querySelector("img"); 
+let img = document.querySelector("img");
 console.log(img.getAttribute("id"));
 // setting the attribute
-console.log(img.setAttribute('src','assets/creation_1.png'));
+console.log(img.setAttribute('src', 'assets/creation_1.png'));
 
 
-let img1 = document.querySelector(".oldImg"); 
+let img1 = document.querySelector(".oldImg");
 
-console.log(img1.setAttribute('src','assets/creation_2.jpeg'));
+console.log(img1.setAttribute('src', 'assets/creation_2.jpeg'));
 
 // using style properety in DOM
 // but we cannot  use  more this style property in our js file because it is use 
 // more inline css .
 // to avoid style manupulating we use classObj
 let links = document.querySelectorAll(".box a")
-for (link of links){
+for (link of links) {
     link.style.color = "purple";
 }
 
-let linked  = document.querySelectorAll("body");
-for (body of linked){
+let linked = document.querySelectorAll("body");
+for (body of linked) {
     body.style.backgroundColor = "lightBlue";
 }
 
 // example of the class list
-let heading  = document.querySelector('h1');
+let heading = document.querySelector('h1');
 heading.classList.add("green");
+
+
 // heading.classList.remove("green"); // to remove the class list  
 //note:Cannot use the set attributes in place of the 
 //classObj.
 
-console.log(heading.classList)  
+console.log(heading.classList)
 
 // navigation in js
 // 1. parentelement 
@@ -51,7 +55,45 @@ console.log(heading.classList)
 // 3.PreviousElement/nextElementsibling
 let nav = document.querySelector(".box");
 
-console.log(nav.children);
-console.log(nav.parentElement);
+console.log(nav.children);  // children
+console.log(nav.parentElement); // parentElement
+
 let nav1 = document.querySelector(" li ul");
-console.log(nav1.children[0].nextElementSibling)
+console.log(nav1.children[0].nextElementSibling) //nxtElementSibling.
+
+// Adding elements
+
+let newp = document.createElement('p') // create the new element
+newp.innerText = "hi i am the dr strange";
+let bodies = document.querySelector('body');
+bodies.appendChild(newp);
+bodies.prepend("hi i am time controller");
+bodies.append("and i am also a neurolisgist")
+
+let newbtn = document.createElement('button')
+newbtn.innerText = "click me";
+let bodie = document.querySelector('.box');
+bodie.appendChild(newbtn);
+newbtn.remove(); // to remove the element use the created variable  to remove.
+
+
+let newhead = document.createElement("h1");
+newhead.innerText = "AND this is all about the marvel spiderMAN"
+let bod = document.querySelector("body");
+bod.appendChild(newhead);
+
+
+
+// here i cereated a button 
+let newbtn1 = document.createElement('button')
+newbtn1.innerText = "newButton";
+
+newhead.insertAdjacentElement('afterend', newbtn1);
+newbtn1.remove();
+
+
+
+
+
+
+
